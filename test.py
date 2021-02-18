d48265faaa0f00550341ebb4a3ac2161410ddfaf
VincentH23 <vincent.huang@centrale-marseille.fr>
2021-02-18 22:03:56 +0000

    version final3.0

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def testing(generatorTest,model):
 
     loss_and_metrics=model.evaluate(generatorTest, batch_size=TEST_DATASET_SIZE)
 
-    print('Erreur de la base de données TEST',loss_and_metrics[0])
+    print('Loss sur la base de données TEST',loss_and_metrics[0])
     print('Taux de reconnaisance',loss_and_metrics[1])
 
     print("Nombre d'opérateurs nécessaires par ligne (base de données de test)")
